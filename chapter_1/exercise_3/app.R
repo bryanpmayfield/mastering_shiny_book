fee713e2a4b7add2ce3b114e96e21dbe9a3cd677
Bryan Mayfield <bryanmayfield@PA-Mac-05.attlocal.net>
2023-12-05 12:24:40 -0500

    Completed chapter 1 exercises

--- chapter_1/exercise_3/app.R
+++ chapter_1/exercise_3/app.R
@@ -1,23 +1,24 @@
 
+
 library(shiny)
+
 
 ui <- fluidPage(
 
-  sliderInput("x", label = "If x is", min =1, max = 50, value = 30),
-  "then x times 5 is",
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  sliderInput("y", label = "and y is", min = 1, max = 50, value = 5),
+  "then x times y is",
   textOutput("product")
+  
   
 )
 
 server <- function(input, output, session) {
 
-  # x <- get(input$x)
-  # The above did not work
-  
   output$product <- renderText({
-    input$x * 5
+    input$x * input$y
   })
-  
+
 }
 
 # Run the application 
